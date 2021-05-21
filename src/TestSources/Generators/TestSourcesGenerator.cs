cb9c595d7e07b907f65fb076032f1581eae83341
José Luis Latorre Millas <JoseLuis.LatorreMillas@swisslife.ch>
2021-05-21 14:59:35 +0200

    baby steps with source generators...

--- src/TestSources/Generators/TestSourcesGenerator.cs
+++ src/TestSources/Generators/TestSourcesGenerator.cs
@@ -7,35 +7,39 @@ using Microsoft.CodeAnalysis.Text;
 namespace TestSources.Generators
 {
     [Generator]
-    public class HelloWorldGenerator : ISourceGenerator
+    public class TestSourcesGenerator : ISourceGenerator
     {
         public void Execute(GeneratorExecutionContext context)
         {
             // begin creating the source we'll inject into the users compilation
             var sourceBuilder = new StringBuilder(@"
 using System;
-namespace HelloWorldGenerated
+
+namespace TestSourcesGenerated
 {
-    public static class HelloWorld
+    public static class TestSources
     {
-        public static void SayHello() 
+        public static void ExploreTestSources() 
         {
-            Console.WriteLine(""Hello from generated code!"");
-            Console.WriteLine(""The following syntax trees existed in the compilation that created this program:"");
+            Console.WriteLine(""Exploration of Test Sources!"");
 ");
+
+            sourceBuilder.AppendLine($@"Console.WriteLine(@""Exploring context.AdditionalFiles"");");
+            foreach (AdditionalText contextAdditionalFile in context.AdditionalFiles)
+            {
+                sourceBuilder.AppendLine($@"Console.WriteLine(@"" - {contextAdditionalFile.Path}"");");
+
+            }
 
             // using the context, get a list of syntax trees in the users compilation
             IEnumerable<SyntaxTree> syntaxTrees = context.Compilation.SyntaxTrees;
 
+            sourceBuilder.AppendLine($@"Console.WriteLine(@""Exploring context.FilePath"");");
             // add the filepath of each tree to the class we're building
             foreach (SyntaxTree tree in syntaxTrees)
             {
                 sourceBuilder.AppendLine($@"Console.WriteLine(@"" - {tree.FilePath}"");");
             }
-
-
-            // simple test
-            //sourceBuilder.AppendLine($@"// blabla... ");
 
             // finish creating the source to inject
             sourceBuilder.Append(@"
@@ -44,7 +48,7 @@ namespace HelloWorldGenerated
 }");
 
             // inject the created source into the users compilation
-            context.AddSource("helloWorldGenerator", SourceText.From(sourceBuilder.ToString(), Encoding.UTF8));
+            context.AddSource("TestSourcesGenerator", SourceText.From(sourceBuilder.ToString(), Encoding.UTF8));
         }
 
         public void Initialize(GeneratorInitializationContext context)
